ee50a127ffbbe9bf694870abb695e98832f92748
SihyeonHong <hongsihyeon179@gmail.com>
2025-08-28 11:34:01 +0900

    feat: SupportLink 컴포넌트 생성

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -8,6 +8,8 @@ import DisplayMode from "@/app/components/common/DisplayMode";
 import LocaleSwitcher from "@/app/components/common/LocaleSwitcher";
 import { Link } from "@/i18n/routing";
 
+import SupportLink from "../SupportLink";
+
 type HeaderType = "admin" | "public" | "root";
 
 interface HeaderProps {
@@ -18,18 +20,24 @@ export default function Header({ type }: HeaderProps) {
   const t = useTranslations("header");
 
   return (
-    <div className="flex w-full flex-col items-end justify-end p-1 sm:flex-row sm:items-center sm:gap-4">
-      <div className="flex">
-        <DisplayMode />
-        <LocaleSwitcher />
-      </div>
+    <div className="flex w-full flex-col items-center justify-between p-1 sm:flex-row sm:items-start sm:gap-4">
+      {/* 왼쪽 */}
+      <SupportLink />
 
-      {type === "public" && (
-        <Button asChild>
-          <Link href={`/`}>{t("loginOrSignup")}</Link>
-        </Button>
-      )}
-      {type === "admin" && <AdminHeader />}
+      {/* 오른쪽 */}
+      <div className="flex flex-col items-center sm:flex-row sm:gap-4">
+        <div className="flex">
+          <DisplayMode />
+          <LocaleSwitcher />
+        </div>
+
+        {type === "public" && (
+          <Button asChild>
+            <Link href={`/`}>{t("loginOrSignup")}</Link>
+          </Button>
+        )}
+        {type === "admin" && <AdminHeader />}
+      </div>
     </div>
   );
 }
